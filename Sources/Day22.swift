5d464e6ce096160c9c593191253983e083f5eeb0
Philipp <pd95@users.noreply.github.com>
2024-12-23 11:01:06 +0100

    Implemented and successfully completed Day 22

--- Sources/Day22.swift
+++ Sources/Day22.swift
@@ -8,6 +8,8 @@ struct Day22: AdventDay {
     var initialSecret: Int
     var numbersGenerated: Int = 0
     var secret: Int
+    var priceChange: Int = 0
+    var priceChanges: [Int] = []
 
     init(initialSecret: Int) {
       self.initialSecret = initialSecret
@@ -24,15 +26,26 @@ struct Day22: AdventDay {
 
     @discardableResult
     mutating func next() -> Int {
+      let initialPrice = price
       secret = prune(mix(secret: secret, with: secret * 64))
       secret = prune(mix(secret: secret, with: secret / 32))
       secret = prune(mix(secret: secret, with: secret * 2048))
       numbersGenerated += 1
+      priceChange = price - initialPrice
+      if priceChanges.count == 4 {
+        priceChanges.removeFirst()
+      }
+      priceChanges.append(priceChange)
+      //print(self)
       return secret
     }
 
+    var price: Int {
+      secret % 10
+    }
+
     var description: String {
-      "\(initialSecret): \(secret) (after \(numbersGenerated) numbers)"
+      "\(initialSecret): \(price) (\(priceChange)) after \(numbersGenerated) numbers"
     }
   }
 
@@ -59,6 +72,47 @@ struct Day22: AdventDay {
   // Replace this with your solution for the second part of the day's challenge.
   func part2() -> Int {
     var result = 0
+
+    var generators: [PseudoRandomNumberGenerator] = data.components(separatedBy: .newlines)
+      .compactMap(Int.init)
+      .map(PseudoRandomNumberGenerator.init)
+
+    // For each random number generator, generate 2000 entries
+    print("Collecting sequences for \(generators.count) generators")
+    var sequences: [[Int]: [(generatorIndex: Int, price: Int)]] = [:]
+    for _ in 0..<2000 {
+
+      generators.indices.forEach({ index in
+        // generate next number
+        generators[index].next()
+
+        // Check whether we have a sequence of 4 price changes
+        let priceChanges = generators[index].priceChanges
+        guard priceChanges.count == 4 else { return }
+
+        // Check if we haven't already had the same sequence for this generator
+        guard sequences[priceChanges] == nil || sequences[priceChanges, default: []].firstIndex(where: { $0.generatorIndex == index }) == nil else {
+          return
+        }
+
+        // Remember the generator and the price
+        sequences[priceChanges, default: []].append((index, generators[index].price))
+      })
+    }
+
+    print("number of sequences", sequences.count)
+
+    // For each sequence, calculate the number of generators and the total number of bananas and sort it
+    let occurences = sequences.mapValues { entry in
+      let total = entry.reduce(0, { $0 + $1.price })
+      return (matches: entry.count, bananas: total)
+    }
+    .sorted(by: { $0.value.1 > $1.value.1 })
+
+    //print("number of sequence occurrences", occurences.map(\.value).reduce(0, +))
+    //print("Top 10 occurences", occurences[..<10])
+
+    result = occurences.first!.value.bananas
 
     return result
   }
--- Tests/Day22.swift
+++ Tests/Day22.swift
@@ -13,6 +13,13 @@ struct Day22Tests {
     2024
     """
 
+  let testData2 = """
+    1
+    2
+    3
+    2024
+    """
+
   @Test func testPseudoRandomNumberGenerator() async throws {
     var generator = Day22.PseudoRandomNumberGenerator(initialSecret: 123)
 
@@ -20,16 +27,47 @@ struct Day22Tests {
     #expect(generator.mix(secret: 42, with: 15) == 37)
     #expect(generator.prune(100000000) == 16113920)
 
+    #expect(generator.price == 3)  // Initial price
+
     #expect(generator.next() == 15887950)
+    #expect(generator.price == 0)
+    #expect(generator.priceChange == -3)
+
     #expect(generator.next() == 16495136)
+    #expect(generator.price == 6)
+    #expect(generator.priceChange == 6)
+
     #expect(generator.next() == 527345)
+    #expect(generator.price == 5)
+    #expect(generator.priceChange == -1)
+
     #expect(generator.next() == 704524)
+    #expect(generator.price == 4)
+    #expect(generator.priceChange == -1)
+
     #expect(generator.next() == 1553684)
+    #expect(generator.price == 4)
+    #expect(generator.priceChange == 0)
+
     #expect(generator.next() == 12683156)
+    #expect(generator.price == 6)
+    #expect(generator.priceChange == 2)
+
     #expect(generator.next() == 11100544)
+    #expect(generator.price == 4)
+    #expect(generator.priceChange == -2)
+
     #expect(generator.next() == 12249484)
+    #expect(generator.price == 4)
+    #expect(generator.priceChange == 0)
+
     #expect(generator.next() == 7753432)
+    #expect(generator.price == 2)
+    #expect(generator.priceChange == -2)
+
     #expect(generator.next() == 5908254)
+    #expect(generator.price == 4)
+    #expect(generator.priceChange == 2)
   }
 
   @Test func testPart1() async throws {
@@ -42,13 +80,13 @@ struct Day22Tests {
     #expect(String(describing: challenge.part1()) == "14691757043")
   }
 
-//  @Test func testPart2() async throws {
-//    let challenge = Day22(data: testData)
-//    #expect(String(describing: challenge.part2()) == "0")
-//  }
-//
-//  @Test func testRealPart2() async throws {
-//    let challenge = Day22()
-//    #expect(String(describing: challenge.part2()) == "0")
-//  }
+  @Test func testPart2() async throws {
+    let challenge = Day22(data: testData2)
+    #expect(String(describing: challenge.part2()) == "23")
+  }
+
+  @Test func testRealPart2() async throws {
+    let challenge = Day22()
+    #expect(String(describing: challenge.part2()) == "1831")
+  }
 }
